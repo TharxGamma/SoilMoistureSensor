83a1eb0801d7a56b380f2431389cd52e8c01b0c2
Tharx <tharxs_gamma@hotmail.com>
2022-05-28 14:36:45 +0100

    initial commit

---
+++ SoilMoisture.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define SensorPin 2
+
+void Display(int);
+
+// Parameters as follows. Starting location of screen and size of LCD screen
+LiquidCrystal_I2C LCD(0x27, 16, 2);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  LCD.init();
+  LCD.backlight();
+  
+  pinMode(SensorPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(digitalRead(SensorPin));
+  int SensorResult = digitalRead(SensorPin);
+  delay(500);
+  Display(SensorResult);
+
+}
+
+void Display(int S)
+{
+  LCD.setCursor(0, 0); // Set the cursor on the first column and first row.
+  if(S == 1)
+  {
+    LCD.print("Soil is dry");
+  } else {
+    LCD.print ("Soil is wet");
+  }
+
+}
